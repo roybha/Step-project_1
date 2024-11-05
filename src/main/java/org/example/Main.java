455a110a7b7bf608ac6826d35ffd571af0eb99c0
Андрій Рубан <dombldor15@gmail.com>
2024-11-05 20:35:16 +0300

    Action commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
         }
     }
